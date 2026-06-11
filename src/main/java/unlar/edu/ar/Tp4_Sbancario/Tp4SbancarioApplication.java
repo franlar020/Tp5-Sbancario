05b29dfa1697044052491012ae32bdea7876f1e5
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-10 21:32:32 -0300

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/unlar/edu/ar/Tp4_Sbancario/Tp4SbancarioApplication.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.Tp4_Sbancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4SbancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4SbancarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unlar/edu/ar/Tp4_Sbancario/Tp4SbancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.Tp4_Sbancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4SbancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
