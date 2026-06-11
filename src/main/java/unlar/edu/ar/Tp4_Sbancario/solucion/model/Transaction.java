a8299c50f9a07020f558a1f47219b36ddae857ec
VirginiaVeraH <virginiavh454@gmail.com>
2026-06-10 23:08:45 -0300

    Entidad inmutable que representa una transacción bancaria refactorizada.

--- src/main/java/unlar/edu/ar/Tp4_Sbancario/solucion/model/Transaction.java
+++ src/main/java/unlar/edu/ar/Tp4_Sbancario/solucion/model/Transaction.java
@@ -1,5 +1,28 @@
 package unlar.edu.ar.Tp4_Sbancario.solucion.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Transaction {
 
+    private final Long id;
+    private final TransactionType tipo;
+    private final double monto;
+    private final LocalDate fecha;
+    private final String descripcion;
+
+    public Transaction(Long id, TransactionType tipo, double monto,
+                       LocalDate fecha, String descripcion) {
+        this.id = id;
+        this.tipo = tipo;
+        this.monto = monto;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+    }
 }
